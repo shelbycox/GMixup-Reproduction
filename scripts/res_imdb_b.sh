87535d1a63d165b3236b6c4d0518c40067882fde
shelbycox <aanford@gmail.com>
2023-05-09 20:43:35 -0400

    Added scripts

---
+++ scripts/res_imdb_b.sh
@@ -0,0 +1,9 @@
+for seed in 1314 11314 21314 31314 41314 51314 61314 71314 81314 91314
+do
+    for r in -15 -10 -5 0 5 10 15
+    do
+      CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GCN --dataset IMDB-BINARY \
+          --lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+          --aug_ratio 0.15 --aug_num 10  --ge USVT --epoch 300 --res=$r
+    done
+done
---
+++ scripts/run_gmixup_PROTEINS.sh
@@ -0,0 +1,7 @@
+
+for seed in 1314 11314 21314 31314 41314 51314 61314 71314 81314 91314
+do 
+    CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GCN --dataset PROTEINS \
+        --lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+        --aug_ratio 0.2 --aug_num 10  --ge USVT --epoch 500 --res=0 --lam_range="[0.1, 0.2]"
+done
